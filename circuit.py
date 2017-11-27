1ff086253e23063287d590f228196f49d5454130
Alex Kassil <alexkassil@sbcglobal.net>
2017-11-26 16:03:14 -0800

    Add eval to Circuit

--- circuit.py
+++ circuit.py
@@ -1,19 +1,27 @@
 from reader import *
-from helpers import *
+#from helpers import *
+import operator
 
 class Circuit:
-    inputs = None
-    circuit = None
     def __init__(self, inputs, circuit):
         self.inputs = remove_duplicates(inputs)
-        self.circuit = mass_circuit(circuit, [['+', 'not ({d[1]} and {d[0]})', 2]])
-#        self.circuit = mass_update(circuit, [['+', 'or'], ['*', 'and'], ['!', 'not']])
+        self.circuit = circuit
+        
+    def __initOLD__(self, inputs, circuit):
+        self.inputs = remove_duplicates(inputs)
+        self.circuit = mass_update(circuit, [['+', 'or'], ['*', 'and'], ['!', 'not']])
 
     def logic_gate(self):
         longest = max(len(self.inputs[0]), 3)
-        display_first(self.inputs, self.circuit, longest)
+        display_first(self.inputs, longest)
         print(('-' * (longest + 3)) * (len(self.inputs)+1))
-        display_rest(self.inputs, self.circuit, longest)
+        display_rest(self.inputs, self, longest)
+
+    def eval(self, args):
+        if len(args) != len(self.inputs):
+            raise ValueError("Mismatched Number of Inputs")
+        return eval(' '.join(mass_update(self.circuit, zip(self.inputs, args))))
+        
         
     def test(self):
         circuit = self.circuit
@@ -22,6 +30,68 @@ class Circuit:
             circuit = update(circuit, inpt, 'True')
 
         print(' '.join(circuit), eval(' '.join(circuit)))
+
+def c_and(x, y):
+    return x and y
+
+def c_not(x):
+    return not x
+
+def update(tokens, old, new):
+    return [new if x == old else x for x in tokens]
+
+def mass_update(tokens, pairs):
+    for old, new in pairs:
+        tokens = update(tokens, old, new)
+    return tokens
+
+def to_true_false(digit, n):
+    """ Turn digit into n True/False values
+    based on each bit in digit.
+    >>> to_true_false(3, 4)
+    [False, False, True, True]
+    """
+    result = []
+    while n:
+        current = pow(2, n-1)
+        if ((digit - current) >= 0):
+            result.append('True')
+            digit -= current
+        else:
+            result.append('False')
+        n -= 1
+    return result
+
+def display_first(inputs, longest):
+    for inpt in inputs:
+        print(inpt, end='')
+        print(' ' * (longest - len(inpt) + 1), end='')
+        print('| ', end='')
+    print('out')
+
+def display_rest(inputs, circuit, longest):
+    n = len(inputs)
+    for binary_digit in range(pow(2, n)):
+        values = to_true_false(binary_digit, n)
+        display_row(values, circuit.eval(values), longest)
+
+def display_row(values, output, longest):
+    for value in values:
+        print(1 if eval(value) else 0, end='')
+        print(' ' * (longest-1), '| ', end='')
+    print(1 if output else 0)
+        
+def remove_duplicates(inputs):
+    result = [inputs[0]]
+    for inpt in inputs:
+        if inpt not in result:
+            result.append(inpt)
+    return result
+
+
+
+
+
 
 
 
--- repl.py
+++ repl.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
             circuit = tokenize(user_input)
             inputs = find_inputs(circuit)
             test = Circuit(inputs, circuit)
-#            test.logic_gate()
+            print(test.inputs, test.circuit)
+            test.logic_gate()
         except (SyntaxError) as err:
             print(type(err).__name__ + ':', err)
         except (KeyboardInterrupt, EOFError):
